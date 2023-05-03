0630f79159db28c72b48ff9c1d053c00128157fd
pratyushsingha <pratyushsingha83@gmail.com>
2023-05-03 20:00:34 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-red-600 flex justify-center items-center">pratyushsingha</div>
+    </>
+  )
+}
+
+export default App
